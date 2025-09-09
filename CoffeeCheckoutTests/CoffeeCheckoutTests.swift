f1e7b524c30a7bf3bc26d49e96ff2d565e8e3942
vladalexbarbatescu-sudo <vladalex.barbatescu@justeattakeaway.com>
2025-09-09 13:20:03 +0200

    Initial Commit

---
+++ CoffeeCheckoutTests/CoffeeCheckoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeCheckoutTests.swift
+//  CoffeeCheckoutTests
+//
+//  Created by Vlad Alex Barbatescu on 09/09/2025.
+//
+
+import Testing
+@testable import CoffeeCheckout
+
+struct CoffeeCheckoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
